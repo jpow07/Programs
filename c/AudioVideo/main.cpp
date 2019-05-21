67f6027a26affff015c39ae7d1b8280bae97668d
Jordan Powell <jpow7@outlook.com>
2019-05-21 15:01:55 -0400

    added opencv code to grab input from camera, Error: Failed to convert mat to frame

--- c/AudioVideo/main.cpp
+++ c/AudioVideo/main.cpp
@@ -4,13 +4,13 @@
  *
  */
 #include <iostream>
-#include "AV_Writer.hpp"
+#include "VideoWriter.hpp"
 
 int main(int argc, char *argv[]) {
 	std::string fileName = "test.mp4";
 	std::string codecName = "libx264";
 	std::cout << "Print" << std::endl;
-	AV_Writer muxer(fileName.c_str(), codecName.c_str(), 344, 288, 30);
+	libav::VideoWriter muxer(fileName.c_str(), codecName.c_str(), 344, 288, 30);
 	
 
 
